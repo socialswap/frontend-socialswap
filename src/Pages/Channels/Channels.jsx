99e16c34d1cdf1c8a797f5c1017f43bf26360fc3
socialswap <socialswapofficial25@gmail.com>
2025-10-29 12:40:28 +0000

    env

--- src/Pages/Channels/Channels.jsx
+++ src/Pages/Channels/Channels.jsx
@@ -17,7 +17,7 @@ const Channels = () => {
   useEffect(() => {
     const fetchChannels = async () => {
       try {
-        const response = await axios.get(`${REACT_APP_API_BASE_URL}/channels`);
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/channels`);
         setChannels(response.data);
         setFilteredChannels(response.data);
       } catch (error) {
